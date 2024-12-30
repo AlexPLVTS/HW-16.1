f2ab0a356d77cdc6be803aaa8519f540bc7fc330
Oleksandr Pyliavets <Oleksandr Pyliavets@DESKTOP-7JRVHFS>
2024-12-30 13:09:38 +0200

    Add project files.

---
+++ HW  16.1/16.1.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the path to input file: ");
+        string inputFilePath = Console.ReadLine();
+
+        Console.Write("Enter the path to the file to copy in: ");
+        string destinationFilePath = Console.ReadLine();
+
+        try
+        {
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine("Input file is not found");
+                return;
+            }
+            File.Copy(inputFilePath, destinationFilePath, true);
+            Console.WriteLine("File is successfully copied");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
